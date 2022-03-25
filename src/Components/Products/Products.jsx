f738b40c3c592489e5c4e67a49a455f636b1c9e1
sadik <po480373@gmai.com>
2022-03-26 00:10:23 +0600

    mapping & display in ui

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import'./Products.css'
+const Products = (props) => {
+    console.log(props);
+    const {price,name,id,picture} = props.pd;
 
-const Products = () => {
     return (
-        <div>
-            <h1>hello im products</h1>
+        <div className='products'>
+            <img src={picture}/>
+            <h1>{name}</h1>
+            <h2>{price}</h2>
+            <button>Add to Cart</button>
         </div>
     );
 };
--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -1,21 +1,30 @@
-import React from 'react';
-import Cart from '../Cart/Cart';
+import React from "react";
+import { useState, useEffect } from "react";
+import Cart from "../Cart/Cart";
 
-import Products from '../Products/Products';
+import Products from "../Products/Products";
 
 const Shop = () => {
-    return (
-        <div>
-            <h1>im from shop</h1>
-           <div className="product">
-               <Products/>
-           </div>
-           <div className="cart">
-               <Cart/>
-           </div>
-
-        </div>
-    );
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+      fetch("cardb.json")
+      .then(res=>res.json())
+      .then(data=>setProducts(data))
+  }, []);
+  return (
+    <div>
+      <h1>im from shop</h1>
+      <div className="product">
+          {
+              products.map(pd=><Products pd={pd} key={pd.id}/>)
+              
+          }
+      </div>
+      <div className="cart">
+        <Cart />
+      </div>
+    </div>
+  );
 };
 
 export default Shop;
